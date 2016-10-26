0a0ef9e26b4378aed3893b00b163bd361f0d3521
noahgribbin <noahgribbin@yahoo.com>
2016-10-26 11:26:25 -0700

    add images to template and style for mobile and desktop

--- project.js
+++ project.js
@@ -2,6 +2,7 @@ var projects = [];
 function Project (parts){
 	this.title = parts.title;
 	this.body = parts.body;
+	this.pic = parts.pic;
 	this.category = parts.category;
 };
 Project.prototype.toHtml = function(){
@@ -9,6 +10,7 @@ Project.prototype.toHtml = function(){
 	$newproject.attr('data-category', this.category);
 	$newproject.find('h1').text(this.title);
 	$newproject.find('section.project-description').html(this.body);
+	$newproject.find('.project-pic').attr('src', this.pic);
 	$newproject.removeClass('template');
 	return $newproject;
 }
