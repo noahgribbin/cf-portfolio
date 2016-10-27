792dcbcf7390fcf81c8a8a7a11555f461b4ef017
noahgribbin <noahgribbin@yahoo.com>
2016-10-27 09:40:43 -0700

    implement naviagtion

--- projectView.js
+++ projectView.js
@@ -7,4 +7,13 @@ projectView.populateFilters = function(){
 		$('#category-filter').append(optionTag)
 	});
 }
+projectView.navigation = function(){
+	$('.main-nav').on('click', '.tab', function(){
+		$('.tab-content').hide()
+		var connector = $(this).attr('data-content');
+		$('#'+connector).fadeIn();
+	});
+	$('.main-nav .tab:first').click
+}
+projectView.navigation();
 projectView.populateFilters();
