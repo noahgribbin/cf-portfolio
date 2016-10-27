2ceb45d6fd0e5f0ec43fe55b46d048d3744697ae
noahgribbin <noahgribbin@yahoo.com>
2016-10-27 10:32:02 -0700

    show and hide projects with filters

--- projectView.js
+++ projectView.js
@@ -13,7 +13,30 @@ projectView.navigation = function(){
 		var connector = $(this).attr('data-content');
 		$('#'+connector).fadeIn();
 	});
-	$('.main-nav .tab:first').click
+	$('.main-nav .tab:first').click();
 }
+projectView.setTeaser = function(){
+	$('.project-description *:nth-of-type(n+2)').hide();
+	$('#projects').on('click', 'a.read-on', function(event){
+		event.preventDefault();
+		$(this).parent().find('*').fadeIn();
+		$(this).hide()
+	})
+}
+projectView.handleCategoryFilter = function() {
+	$('#category-filter').on('change', function(){
+		if ($(this).val()) {
+			$('article').hide();
+			var categoryConnector = $(this).val();
+			$('article[data-category="'+categoryConnector+'"]').fadeIn();
+		}
+		else {
+			$('article').not('.template').show();
+		}
+	})
+}
+
+projectView.setTeaser();
 projectView.navigation();
 projectView.populateFilters();
+projectView.handleCategoryFilter();
